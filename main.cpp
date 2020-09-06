f22843c483908dc6e0fba312ae678f7c7638f1a3
notfreshmbp <1195056983@qq.com>
2020-09-06 17:04:08 +0800

    虚函数的重写问题
    如果想使用派生类原本的成员呢？

--- main.cpp
+++ main.cpp
@@ -1,73 +1,31 @@
 #include <iostream>
 #include <map>
 using namespace std;
-void testLeftValue();
-void testMap();
-void TestOperater();
-
-#include "test1.h"
-#include "test2.h"
 
 
+class A{
+public:
+    int m_abc = 1;
+    virtual void f1(int n){
+        cout << "A.f1," << n << ", abc:" <<  abc << endl;
+    }
+};
+class B: public A{
+public:
+    int abc = 2;
+    virtual void f1(){
+        cout << "B.f1," << endl;
+    }
+
+    virtual void f1(int n){
+        this->A::f1(n);
+    }
+};
 
 int main() {
     cout << "Hello, World!" << endl;
-    // testLeftValue();
-    //testMap();
-     //findNum();
-//     int a = 10;
-//     func(a);
-//
-    test01();
-
-
-
+    A *b = new B;
+    b->f1(1);
     return 0;
-}
-
-void test01()
-{
-    Son s;
-
-    cout << "Son下的m_A = " << s.m_A << endl;
-    cout << "Base下的m_A = " << s.Base::m_A << endl;
-
-    s.func();
-    s.Base::func();
-    s.Base::func(10);
-
-    cout << "Son下的m_A = " << s.m_A << endl;
-    cout << "Base下的m_A = " << s.Base::m_A << endl;
-}
-
-
-int& f1(){
-    static int a = 10;
-    return a;
-}
-
-void testLeftValue(){
-    int &b = f1();
-    f1() = 20;
-    cout << b << endl;
-}
-
-void TestOperater(){
-    A a1, a2;
-    a1.a = 10;
-    a2.a = 11;
-    A a3 = a1 + a2;
-    cout << a3.a << endl;
-}
-
-void testMap(){
-    map<char,int> first;
-    first['a'] = 1;
-    first['b'] = 2;
-    first['c'] = 3;
-
-    for (map<char,int>::iterator it=first.begin(); it!=first.end(); ++it)
-        std::cout << it->first << ":" << it->second << '\n';
-
 }
 
--- test2.cpp
+++
@@ -1,6 +0,0 @@
-//
-// Created by zxzx on 2020/9/4.
-//
-using namespace std;
-
-#include <iostream>
