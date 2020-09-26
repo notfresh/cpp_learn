386e87ff4c4f047f4c2b22022969797e9ab69e52
notfreshmbp <1195056983@qq.com>
2020-09-26 15:45:26 +0800

    创建两个线程

--- main.cpp
+++ main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
+#include <thread>
 #include <map>
 using namespace std;
 
-
+void f1(){
+    cout << "f1" << endl;
+}
+void f2(int x){
+    cout << "f2" << "," << x << endl;
+}
 
 
 int main() {
     cout << "Hello, World!" << endl;
+    thread t1(f1);
+    thread  t2(f2,10);
+    t2.join();
+    t1.join();
 
-
+    cout << "Hello, World! over" << endl;
 
     return 0;
 }
