9cac5e11a89ca65fa9c3bff7b071666b81b59961
notfreshmbp <1195056983@qq.com>
2020-10-09 07:57:46 +0800

    测试新方法

--- main.cpp
+++ main.cpp
@@ -2,26 +2,11 @@
 #include <map>
 using namespace std;
 
+#include "testExtend.h"
 
-class A{
-public:
-    int m_abc = 1;
-
-};
-
-class B: public A{
-public:
-    int m_abc = 2;
-    const int a = 10;
-    B():a(3){}
-    B(int x){}
-};
 
 int main() {
     cout << "Hello, World!" << endl;
-    B *b = new B;
-    cout << b->a << endl; // 3
-    B *b2 = new B(3);
-    cout << b2->a << endl; // 10
+    test01();
 }
 
