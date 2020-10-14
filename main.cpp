15c949d57617e08ba4fec614f9540203bce873e3
notfreshmbp <1195056983@qq.com>
2020-10-14 09:18:08 +0800

    zzz

--- main.cpp
+++ main.cpp
@@ -1,28 +1,47 @@
 #include <iostream>
-#include <map>
-#include <list>
+//#include <map>
+//#include <list>
 
 using namespace std;
 
-#include <vector>
+//#include <vector>
 
-#include "testExtend.h"
-#include "testList.h"
-#include "testMap.h"
-#include "testAlgorithm.h"
-#include "testGeneric.h"
-#include "testGeneric2.h"
-#include "testGenericSort.h"
+//#include "testExtend.h"
+//#include "testList.h"
+//#include "testMap.h"
+//#include "testAlgorithm.h"
+//#include "testGeneric.h"
+//#include "testGeneric2.h"
+//#include "testGenericSort.h"
+//#include "testDIYSmartPointer.h"
+
+//void main_f1(){cout << 1 << endl;}; // 函数结尾也可有；
+
 
 int main() {
-    cout << "Hello, World!" << endl;
-    //test01();
-    //f2();
-//    map_f1();
-//    al_f1();
-//generic_t1();
-//generic_f1();
-generic_sort_f1();
+////    cout << "Hello, World!" << endl;
+//    //test01();
+//    //f2();
+//    //    map_f1();
+//    //    al_f1();
+//    //generic_t1();
+//    //generic_f1();
+//    //generic_sort_f1();
+//    // int* p = NULL;
+//    // main_f1();
+//
+////    SmartPrt<Test> t1;
+////    t1->show();
+//
+////    SmartPrt<Test> t2(new Test("abc"));
+////    t2->show();
+//    string a = "abc";
+////    Test* t1 = new Test(0);
+////    t1->showName();
+//    cout << a << endl;
+
 }
+
+
 
 
